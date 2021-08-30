690463c3688197d8ab2d6a03665ae9687045d052
Amine Falek <falek@unistra.fr>
2021-08-30 11:43:33 +0200

    Added project description and copyright statement

--- src/structures/EdgeLabeled.java
+++ src/structures/EdgeLabeled.java
@@ -1,4 +1,22 @@
+// Copyright 2021 Technology & Strategy / ICube, GNU Public License.
+
 package structures;
+
+/**
+ * The project contains the implementation of MUSE algorithm for the purpose 
+ * of multimodal route planning. The project requires a dataset available at:
+ * https://doi.org/10.5281/zenodo.5276749
+ * 
+ * MUSE has been sumbitted to the Transportation Science Journal in a paper
+ * entitled "MUSE: Multimodal Separators for Efficient Route Planning in 
+ * Transportation Networks".
+ * 
+ * This project also includes the implementation of the SDALT algorithm as
+ * described in the paper. It allows for reproducing the stated performance
+ * evaluation.
+ *  
+ * @author Amine Falek <a.falek@technologyandstrategy.com>
+ */
 
 public class EdgeLabeled {
 	public int head;
